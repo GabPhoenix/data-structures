3040451d8f35d7d4c70fa7c58a7f17da8c2c26b8
gabphoenix <anjomito26@gmail.com>
2022-03-30 17:02:05 -0300

    updates

--- trees/binaries/Test.java
+++ trees/binaries/Test.java
@@ -4,12 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		Tree tree = new Tree();
-		tree.add(25);
 		tree.add(15);
 		tree.add(10);
+		tree.add(20);
 		tree.add(12);
 		tree.add(5);
-		tree.add(20);
+		tree.add(25);
 		tree.add(18);
 		System.out.println("Pre order:");
 		tree.preOrder(tree.getRoot());
@@ -20,6 +20,11 @@ public class Test {
 		System.out.println("Post order:");
 		tree.postOrder(tree.getRoot());
 		System.out.println("\n");
+		System.out.println("height: " + tree.getHeight());
+		System.out.println(tree.search(tree.getRoot(), 15));
+		System.out.println(tree.search(tree.getRoot(), 12));
+		System.out.println(tree.search(tree.getRoot(), 19));
+		System.out.println(tree.search(tree.getRoot(), 18));
 	}
 
 }
--- trees/binaries/Tree.java
+++ trees/binaries/Tree.java
@@ -37,6 +37,21 @@ public class Tree {
 		}
 	}
 	
+	public boolean search(Node temp, int info) {
+		if(!this.isEmpty()) {
+			if(temp != null) {
+				if(temp.getInfo() == info) {
+					return true;
+				} else if(temp.getInfo() > info) {
+					return search(temp.getLeft(), info);
+				} else if(temp.getInfo() < info) {
+					return search(temp.getRight(), info);
+				}
+			}
+		}
+		return false;
+	}
+	
 	public int getSize() {
 		return this.size++;
 	}
@@ -46,6 +61,13 @@ public class Tree {
 			return true;
 		}
 		return false;
+	}
+	
+	public int getHeight() {
+		if(!this.isEmpty()) {
+			return (int) (Math.log(this.size)/Math.log(2));
+		}
+		return -1;
 	}
 	
 	public Node getRoot() {
@@ -85,4 +107,6 @@ public class Tree {
 			System.out.print("[" + temp.getInfo() + "]");
 		}
 	}
+	
+	
 }
